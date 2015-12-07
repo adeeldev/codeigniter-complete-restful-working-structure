1d045697d21deaf78430d4b40c0ec5905ec37875
innovativebird <adeelbashir5@gmail.com>
2015-12-07 22:39:44 +0500

    first commit

---
+++ application/controllers/Rest_server.php
@@ -0,0 +1,13 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Rest_server extends CI_Controller {
+
+    public function index()
+    {
+        $this->load->helper('url');
+        $this->load->helper('rest');
+        $this->load->view('rest_server');
+    }
+}
